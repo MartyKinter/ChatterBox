0e3f359df4170a4cc5fa1129b6a8c3a347820b44
Marty Kinter <martykinter@Martys-Air.attlocal.net>
2023-08-08 22:53:21 -0400

    testing bug

--- chat-app-client/src/ApiRoutes.js
+++ chat-app-client/src/ApiRoutes.js
@@ -1,7 +1,7 @@
-export const host = process.env.BACKEND_URL;
+export const host = process.env.REACT_APP_BACKEND_URL;
 
 export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${process.env.BACKEND_URL}/api/auth/login`;
+export const loginRoute = `${process.env.REACT_APP_BACKEND_URL}/api/auth/login`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
 export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
